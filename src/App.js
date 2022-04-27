c9ef545b00c76149caaba75519b358dd3cd57873
momolly1024 <momolly1024@gmail.com>
2022-04-27 10:05:49 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
+import MainDash from './components/MainDash/MainDash'
+import RightSide from './components/RightSide/RightSide'
+import Sidebar from './components/Sidebar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+const Test = () => {
+    return <div>qqqqqqqqqqqqqq</div>
+}
 function App() {
-    return <div className='App'>wwwwww</div>
+    return (
+        <div className='App'>
+            <div className='AppGlass'>
+                <Sidebar />
+                <MainDash />
+                <RightSide />
+            </div>
+        </div>
+    )
 }
 
 export default App
