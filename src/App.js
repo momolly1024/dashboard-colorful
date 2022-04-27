f6c736a50734735903c7566c635299060dce9bc2
momolly1024 <momolly1024@gmail.com>
2022-04-27 09:03:21 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
- 
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      wwwwww
-    </div>
-  );
+    return <div className='App'>wwwwww</div>
 }
 
-export default App;
+export default App
